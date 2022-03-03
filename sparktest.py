7be54e318ddaa40901d9281340e8f5f815cd5f09
EMR1 <abiyaz.chowdhury@stonybrook.edu>
2022-03-03 14:51:11 +0000

    Modified ansible scripts for use with EMR

--- sparktest.py
+++ sparktest.py
@@ -2,16 +2,16 @@ from pyspark import SparkContext
 import random
 import string
 import time
-output = open("/home/ubuntu/output.txt","w")
+output = open("/home/hadoop/output.txt","w")
 
 num_words = 100
 word_length = 4
 letters = string.ascii_lowercase
 start_time = time.time()
-sc = SparkContext("spark://slurm-head.cloud.compas.cs.stonybrook.edu:7077","first app")
+sc = SparkContext("spark://ip-172-31-94-126.ec2.internal:7077","first app")
 #sc = SparkContext("local","first app")
 sc.setLogLevel("WARN")
-text_file = sc.textFile("/home/ubuntu/sample.txt")
+text_file = sc.textFile("/home/hadoop/sample.txt")
 counts = text_file.flatMap(lambda line: line.split(" ")) \
              .map(lambda word: (word, 1)) \
              .reduceByKey(lambda a, b: a + b)
