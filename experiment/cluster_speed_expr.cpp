2df373fab4385b6ee4b021c783b6dc0c4229e554
Evan West <evtwest@yahoo.com>
2022-03-12 12:29:49 -0500

    added speed experiment code

---
+++ experiment/cluster_speed_expr.cpp
@@ -0,0 +1,52 @@
+#include <graph_distrib_update.h>
+#include <binary_graph_stream.h>
+
+#include <string>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  GraphDistribUpdate::setup_cluster(argc, argv);
+
+  if (argc != 3) {
+    std::cout << "Incorrect number of arguments. "
+                 "Expected two but got " << argc-1 << std::endl;
+    std::cout << "Arguments are: input_stream, output_file" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  std::string input  = argv[1];
+  std::string output = argv[2];
+
+  BinaryGraphStream stream(input, 32 * 1024);
+
+  node_id_t num_nodes = stream.nodes();
+  long m              = stream.edges();
+  long total          = m;
+  GraphDistribUpdate g{num_nodes};
+
+  auto start = std::chrono::steady_clock::now();
+
+  while (m--) {
+    g.update(stream.get_edge());
+  }
+
+  std::cout << "Starting CC" << std::endl;
+  uint64_t num_CC = g.spanning_forest_query().size();
+  
+  std::chrono::duration<double> runtime = g.flush_end - start;
+  std::chrono::duration<double> CC_time = g.cc_alg_end - g.cc_alg_start;
+
+  std::ofstream out{output,  std::ofstream::out | std::ofstream::app}; // open the outfile
+  std::cout << "Number of connected components is " << num_CC << std::endl;
+  std::cout << "Writing runtime stats to " << output << std::endl;
+
+  // calculate the insertion rate and write to file
+  // insertion rate measured in stream updates 
+  // (not in the two sketch updates we process per stream update)
+  float ins_per_sec = (((float)(total)) / runtime.count());
+  out << "Procesing " << total << " updates took " << runtime.count() << " seconds, " << ins_per_sec << " per second\n";
+
+  out << "Connected Components algorithm took " << CC_time.count() << " and found " << num_CC << " CC\n";
+  out.close();
+
+  GraphDistribUpdate::teardown_cluster();
+}
