e4a475de2def6e8f8b6655c9c1aad909aabf4251
Evan West <evtwest@yahoo.com>
2022-03-26 11:56:14 -0400

    new state for work distributor

--- test/distributed_graph_test.cpp
+++ test/distributed_graph_test.cpp
@@ -6,7 +6,7 @@
 
 TEST(DistributedGraphTest, SmallRandomGraphs) {
   write_configuration(false, false, 4, 1);
-  int num_trials = 5;
+  int num_trials = 1;
   while (num_trials--) {
     generate_stream();
     std::ifstream in{"./sample.txt"};
