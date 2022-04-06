87bd334f0613db2258f2779a53ffdc652f0d3b06
Evan West <evtwest@yahoo.com>
2022-04-06 15:17:53 -0400

    swtich test config

--- test/distributed_graph_test.cpp
+++ test/distributed_graph_test.cpp
@@ -5,7 +5,7 @@
 #include <graph_gen.h>
 
 TEST(DistributedGraphTest, SmallRandomGraphs) {
-  write_configuration(false, false, 512, 1);
+  write_configuration(false, false, 8, 1);
   int num_trials = 5;
   while (num_trials--) {
     generate_stream();
