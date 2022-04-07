3081e2b63152c6aaa580b01a4f6bd139e6fd6050
EC2 Default User <ec2-user@ip-172-31-1-247.ec2.internal>
2022-04-07 19:47:38 +0000

    fixing ip fetching script

--- tools/fetch_ips.sh
+++ tools/fetch_ips.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws ec2 describe-instances --region us-east-1 --no-paginate --filters Name=instance-state-name,Values=running --filters Name=tag:ClusterNodeType,Values=Master | jq -r .Reservations[0].Instances[].NetworkInterfaces[0].PrivateDnsName
-aws ec2 describe-instances --region us-east-1 --no-paginate --filters Name=instance-state-name,Values=running --filters Name=tag:ClusterNodeType,Values=Worker | jq -r .Reservations[0].Instances[].NetworkInterfaces[0].PrivateDnsName
+aws ec2 describe-instances --region us-east-1 --no-paginate --filters Name=instance-state-name,Values=running --filters Name=tag:ClusterNodeType,Values=Master | jq -r .Reservations[].Instances[].NetworkInterfaces[0].PrivateDnsName
+aws ec2 describe-instances --region us-east-1 --no-paginate --filters Name=instance-state-name,Values=running --filters Name=tag:ClusterNodeType,Values=Worker | jq -r .Reservations[].Instances[].NetworkInterfaces[0].PrivateDnsName
 
