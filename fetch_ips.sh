5d3039ee48502d956ed272a86da910e45405e7a2
EC2 Default User <ec2-user@ip-172-31-1-247.ec2.internal>
2022-04-07 02:13:37 +0000

    Script to get all node IPS tagged with ClusterNodeType:Master then ClusterNodeType:Worker

---
+++ fetch_ips.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+aws ec2 describe-instances --region us-east-1 --no-paginate --filters Name=instance-state-name,Values=running --filters Name=tag:ClusterNodeType,Values=Master | jq -r .Reservations[0].Instances[].NetworkInterfaces[0].PrivateDnsName > node_list.txt
+aws ec2 describe-instances --region us-east-1 --no-paginate --filters Name=instance-state-name,Values=running --filters Name=tag:ClusterNodeType,Values=Worker | jq -r .Reservations[0].Instances[].NetworkInterfaces[0].PrivateDnsName >> node_list.txt
+
