805316bbb9d8a46c7f008e41af8be57912dc97c5
Daniel DeLayo <dandelayo@gmail.com>
2022-04-07 19:03:02 +0000

    adding to gitignore + first pass combined setup script

---
+++ tools/setup_tagged_workers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [[ $# -ne 1 ]]; then
+  echo "Invalid arguments. Require num_cpu_per_node"
+  echo "num_cpu_per_node:  The number of CPUs on each worker."
+  exit
+fi
+
+cur_dir=`dirname "$(readlink -f "$0")"`
+
+$cur_dir/fetch_ips.sh > $cur_dir/node_list.txt
+$cur_dir/cluster_basic_init.sh $cur_dir/node_list.txt $1
+ansible-playbook -i inventory.ini $cur_dir/ansible/ssh.yaml &
+ansible-playbook -i inventory.ini $cur_dir/ansible/mpi.yaml &
+ansible-playbook -i inventory.ini $cur_dir/ansible/files.yaml &
+
+cp $cur_dir/hostfile $cur_dir/..
+cp $cur_dir/hostfile $cur_dir/../build
+
+wait
