543aaeccaa04d3c3b582445b8b5b771fdac84a4f
Evan West <evtwest@yahoo.com>
2022-07-13 17:50:39 -0400

    infinite cutoff for testing

--- src/work_distributor.cpp
+++ src/work_distributor.cpp
@@ -11,7 +11,7 @@ bool WorkDistributor::shutdown = false;
 bool WorkDistributor::paused   = false; // controls whether threads should pause or resume work
 int WorkDistributor::num_distributors = 1;
 int WorkDistributor::max_work_distributors = 36;
-size_t WorkDistributor::local_process_cutoff = 800; // TODO: what should this number be?
+size_t WorkDistributor::local_process_cutoff = -1; // TODO: what should this number be?
 node_id_t WorkDistributor::supernode_size;
 WorkDistributor **WorkDistributor::workers;
 std::condition_variable WorkDistributor::pause_condition;
